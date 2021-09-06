aa2da2438ff802e3f42e0af69a472247efef62ec
PaulModaley <pmodaley@gmail.com>
2021-09-06 11:47:07 +0000

    add code for guessing row

--- run.py
+++ run.py
@@ -1,6 +1,6 @@
 from random import randint
 
-#initializing board
+#set up game board
 board = []
 
 for x in range(5):
@@ -8,5 +8,17 @@ for x in range(5):
 
 def print_board(board):
   for row in board:
-    print (" ").join(row)
+    print(" ").join(row)
 
+#code to add battleships to game board
+def random_row(board):
+  return randint(0, len(board) -1)
+
+def random_column(board):
+  return randint(0, len(board) -1)
+
+ship_row = random_row(board)
+ship_col = random_column(board)
+
+#code to prompt user to guess row and allow input
+guess_row = int(input("Guess row: "))
