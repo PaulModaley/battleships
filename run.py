41cbabf2fc86f180f4874ba665a9605defe0fc41
PaulModaley <pmodaley@gmail.com>
2021-09-06 11:39:33 +0000

    continue coding board

--- run.py
+++ run.py
@@ -1,5 +1,13 @@
+from random import randint
+
+#initializing board
 board = []
 
-for i in range(0,5):
-  board.append(["O"]*5)
+for x in range(5):
+  board.append(["O"] * 5)
+
+def print_board(board):
+  for row in board:
+    print (" ").join(row)
+
 print(board)
