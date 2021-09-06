68cc6d87b0fc80237ac66e108b10839506acc131
PaulModaley <pmodaley@gmail.com>
2021-09-06 12:18:23 +0000

    fix error with join function

--- run.py
+++ run.py
@@ -1,25 +1,41 @@
 from random import randint
 
-#set up game board
+# set up game board
 board = []
 
 for x in range(5):
-  board.append(["O"] * 5)
+    board.append(["O"] * 5)
+
 
 def print_board(board):
-  for row in board:
-    print(" ").join(row)
+    for i in board:
+        print((" ").join(i))
 
-#code to add battleships to game board
+print_board(board)
+
+# code to add battleships to game board
 def random_row(board):
-  return randint(0, len(board) -1)
+    return randint(0, len(board) - 1)
+
 
 def random_column(board):
-  return randint(0, len(board) -1)
+    return randint(0, len(board) - 1)
+
 
 ship_row = random_row(board)
 ship_col = random_column(board)
 
-#code to prompt user to guess row and allow input
+# code to prompt user to guess row and allow input
 guess_row = int(input("Guess row: "))
 guess_column = int(input("Guess column: "))
+
+print(ship_row)
+print(ship_col)
+
+# outcome of user's guess
+if guess_row == ship_row and guess_column == ship_col:
+    print("Direct hit! You sank my battleship!")
+else:
+    print("Nice try! You missed my battleship!")
+    board[guess_row][guess_column] = "X"
+    print_board(board)
