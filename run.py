dd8449154fc0281f2bd137240a09c32955368e2d
PaulModaley <pmodaley@gmail.com>
2021-09-07 10:44:50 +0000

    start adding in seperate boards for player and computer

--- run.py
+++ run.py
@@ -1,6 +1,7 @@
 from random import randint
 
 # set up game board
+print("Computer")
 board = []
 
 for x in range(5):
@@ -13,7 +14,21 @@ def print_board(board):
 
 print_board(board)
 
-# code to add battleships to game board
+
+#set up human player board
+print("You")
+player_board = []
+
+for x in range(5):
+    player_board.append(["O"] * 5)
+
+def print_board(player_board):
+    for i in player_board:
+        print((" ").join(i))
+
+print_board(player_board)
+
+# code to add battleships to computer board
 def random_row(board):
     return randint(0, len(board) - 1)
 
@@ -24,6 +39,16 @@ def random_column(board):
 
 ship_row = random_row(board)
 ship_col = random_column(board)
+
+#code to add battleships to player board
+def random_row(player_board):
+    return randint(0, len(player_board) - 1)
+
+def random_column(player_board):
+    return randint(0, len(player_board) - 1)
+
+player_row = random_row(board)
+player_col = random_column(board)
 
 # code to prompt user to guess row and allow input
 guess_row = int(input("Guess row (maximum is 5): "))
